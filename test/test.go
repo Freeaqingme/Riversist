8296a15701b225eaeedc22f1d2bb0510474b3acd
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2014-08-24 23:37:05 +0200

    Added PCAP support, locking of tables

--- test/test.go
+++ test/test.go
@@ -1,3 +1,5 @@
+//todo: exclude own ip's
+
 package main
 
 import (
@@ -8,7 +10,6 @@ import (
 	"flag"
 	"fmt"
 	//    "gcfg"
-	"io"
 	"net"
 	"os"
 	"os/exec"
@@ -17,18 +18,29 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"syscall"
 	"test/log"
+	"time"
 	"unsafe"
 )
 
+type ipMap struct {
+	sync.RWMutex
+	m map[string]int64
+}
+
 var logger log.Logger
+var processedIps ipMap
+var processingIps ipMap
 
 func main() {
 
+	processedIps = ipMap{m: make(map[string]int64)}
+	processingIps = ipMap{m: make(map[string]int64)}
+
 	setProcessName("riversist")
 
-	listen := flag.String("listen", "[::1]:798", "Address:port to listen on")
 	logLevel := flag.Int("loglevel", 5, "Syslog Loglevel (0-7, Emerg-Debug)")
 	flag.Parse()
 
@@ -36,11 +48,14 @@ func main() {
 	logger.Log(log.LOG_NOTICE, "Starting...")
 	defer logger.Log(log.LOG_CRIT, "Exiting...")
 
+	go cleanProcessedIps()
+	go cleanProcessingIps()
+
 	sig := make(chan bool)
 	loop := make(chan error)
 
 	go sigHandler(sig)
-	go server(*listen, loop)
+	go server(loop)
 
 	for quit := false; !quit; {
 		select {
@@ -80,28 +95,6 @@ func sigHandler(q chan bool) {
 	}
 }
 
-func server(listen string, loop chan error) {
-
-	ln, err := net.Listen("tcp", listen)
-	if err != nil {
-		logger.Fatal(err.Error())
-	}
-	defer ln.Close()
-	logger.Log(log.LOG_INFO, "Now listening on", listen)
-
-	for {
-		loop <- nil
-
-		c, err := ln.Accept()
-		if err != nil {
-			logger.Log(log.LOG_WARNING, string(err.Error()))
-			continue
-		}
-
-		go handleServerConnection(c)
-	}
-}
-
 func setProcessName(name string) error {
 	argv0str := (*reflect.StringHeader)(unsafe.Pointer(&os.Args[0]))
 	argv0 := (*[1 << 30]byte)(unsafe.Pointer(argv0str.Data))[:argv0str.Len]
@@ -119,44 +112,83 @@ func setProcessName(name string) error {
 	return nil
 }
 
-func handleServerConnection(c net.Conn) {
-	defer c.Close()
+func server(loop chan error) {
 
-	ip, _, _ := net.SplitHostPort(c.RemoteAddr().String())
-	go addIpToTable(ip)
+	handle, err := pcap.OpenLive("eth0", 128, true, 0)
+	if err != nil {
+		panic(err)
+	}
+	if err := handle.SetBPFFilter("tcp and ip and (port 80 or port 443 or port 21 or port 20)"); err != nil { // optional
+		panic(err)
+	}
 
-	for {
-		msg := make([]byte, 1024)
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		loop <- nil
 
-		n, err := c.Read(msg)
-		if err == io.EOF {
-			logger.Log(log.LOG_DEBUG, fmt.Sprintf("received EOF from %s (%d bytes ignored)", c.RemoteAddr(), n))
-			return
-		} else if err != nil {
-			logger.Log(log.LOG_INFO, fmt.Sprintf("ERROR while reading from %s: %s", c.RemoteAddr(), err))
-			return
+		p := packet
+		net := p.NetworkLayer()
+		if net == nil {
+			logger.Log(log.LOG_WARNING, "No net layer in packet found")
+			continue
 		}
-		logger.Log(log.LOG_DEBUG, fmt.Sprintf("received %v bytes from %s", n, c.RemoteAddr()))
+		ip4 := net.(*layers.IPv4)
+		tcp := p.TransportLayer().(*layers.TCP)
+		srcIp := ip4.SrcIP.String()
+		dstIp := ip4.DstIP.String()
+		logger.Log(log.LOG_DEBUG, fmt.Sprintf("Received packet from %s:%s to %s:%s", srcIp, tcp.SrcPort, dstIp, tcp.DstPort))
 
-		/*        n, err = c.Write(msg[:n])
-		          if err != nil {
-		              logger.Log(syslog.LOG_INFO, fmt.Sprintf("ERROR while writing to %s: %s", c.RemoteAddr(), err))
-		              return
-		          }
-		          logger.Log(syslog.LOG_DEBUG, fmt.Sprintf("Sent %v bytes to %s\n", n, c.RemoteAddr()))
-		*/
+		go processIp(srcIp)
+		go processIp(dstIp)
+
+		_ = &runtime.MemStats{}
+		//            memStats := &runtime.MemStats{}
+		//            runtime.ReadMemStats(memStats)
+		//            fmt.Println("Length:", len(processedIps), runtime.NumGoroutine(), memStats.Alloc, memStats.TotalAlloc)
 	}
 
 }
 
-func addIpToTable(ip string) {
-	ip = "127.0.0.1"
-	ip = "127.1.80.1"
+func processIp(ip string) {
+	curTime := time.Now().Unix()
 
+	processedIps.RLock()
+	if _, ok := processedIps.m[ip]; ok {
+		logger.Log(log.LOG_DEBUG, fmt.Sprintf("%v has already been evaluated. Skipping", ip))
+		processedIps.RUnlock()
+		return
+	}
+	processedIps.RUnlock()
+
+	processingIps.RLock()
+	if _, ok := processingIps.m[ip]; ok {
+		logger.Log(log.LOG_DEBUG, fmt.Sprintf("%v is already being evaluated. Skipping", ip))
+		processingIps.RUnlock()
+		return
+	}
+	processingIps.RUnlock()
+
+	processingIps.Lock()
+	processingIps.m[ip] = curTime
+	processingIps.Unlock()
+
+	addIpToTable(ip)
+
+	processedIps.Lock()
+	processingIps.Lock()
+	processedIps.m[ip] = curTime
+	delete(processingIps.m, ip)
+	processingIps.Unlock()
+	processedIps.Unlock()
+}
+
+func addIpToTable(ip string) {
 	var cmdStr string
 	if isIpHam(ip) {
+		logger.Log(log.LOG_NOTICE, fmt.Sprintf("Evaluated IP. IP: %s, table: ham", ip))
 		cmdStr = "/home/dolf/Projects/Go/addIpToTable ham %s"
 	} else {
+		logger.Log(log.LOG_NOTICE, fmt.Sprintf("Evaluted IP. IP: %s, table: spam", ip))
 		cmdStr = "/home/dolf/Projects/Go/addIpToTable spam %s"
 	}
 
@@ -174,6 +206,8 @@ func addIpToTable(ip string) {
 }
 
 func isIpHam(ip string) bool {
+	_ = fmt.Sprintf("%v", ip)
+	return true // Make sure we do not DDOS project honeypot first
 
 	if strings.Index(ip, ".") < 0 {
 		// As we don't support IPv6 yet, it is all considered HAM
@@ -213,4 +247,49 @@ func isIpHam(ip string) bool {
 
 	logger.Log(log.LOG_INFO, "DNSBL: httpbl.org, IP:", ip, " score:", strconv.Itoa(score), ", threshold:", strconv.Itoa(25), ", verdict: ham, dnsbl_retval:", host[0])
 	return true
+}
+
+func cleanProcessedIps() {
+
+	ticker := time.NewTicker(time.Second * 1800)
+
+	for _ = range ticker.C {
+		logger.Log(log.LOG_INFO, "Cleaning the processedIps map.")
+
+		cleanThreshold := time.Now().Unix() - (24 * 60 * 60)
+
+		counter := 0
+		processedIps.Lock()
+		for k, v := range processedIps.m {
+			if v < cleanThreshold {
+				delete(processedIps.m, k)
+				counter++
+			}
+		}
+		logger.Log(log.LOG_INFO, fmt.Sprintf("Cleaned %v items from processedIps. Current size is: %v", counter, len(processedIps.m)))
+		processedIps.Unlock()
+	}
+}
+
+func cleanProcessingIps() {
+
+	ticker := time.NewTicker(time.Second * 60)
+
+	for _ = range ticker.C {
+		logger.Log(log.LOG_INFO, "Cleaning the processingIps map.")
+
+		cleanThreshold := time.Now().Unix() - 60
+
+		counter := 0
+		processingIps.Lock()
+		for k, v := range processingIps.m {
+			if v < cleanThreshold {
+				delete(processingIps.m, k)
+				counter++
+			}
+		}
+		logger.Log(log.LOG_INFO, fmt.Sprintf("Cleaned %v items from processingIps. Current size is: %v", counter, len(processingIps.m)))
+		processingIps.Unlock()
+
+	}
 }
