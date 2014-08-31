8dc215709483a0e58a54bf32a669d52a3fc1a42c
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2014-08-31 23:57:15 +0200

    Adding license header in main .go file

--- riversist.go
+++ riversist.go
@@ -1,3 +1,10 @@
+// Riversist - Monitor and firewall network traffic based on DNSBLs
+//
+// Copyright 2014 Dolf Schimmel, Freeaqingme.
+//
+// This Source Code Form is subject to the terms of the two-clause BSD license.
+// For its contents, please refer to the LICENSE file.
+//
 package main
 
 import "riversist/riversistd"
